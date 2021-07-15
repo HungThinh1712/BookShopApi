3773477ae714a62c461bc2bff6cebfdc7f3e0555
Thinh Huynh <thinh.huynh@bigin.vn>
2021-07-15 17:41:05 +0700

    update api

--- BookShopApi/Startup.cs
+++ BookShopApi/Startup.cs
@@ -35,7 +35,8 @@ namespace BookShopApi
                 options.AddPolicy("AllowAllOriginsPolicy",
                 builder =>
                 {
-                    builder.SetIsOriginAllowed(origin => true)
+                    builder.WithOrigins("https://bookshoptina.herokuapp.com",
+                                                      "http://localhost:3000")
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials();
@@ -97,7 +98,6 @@ namespace BookShopApi
 
             app.UseRouting();
             app.UseCors("AllowAllOriginsPolicy");
-            app.UseAuthorization();
             app.UseSession();       
           
             app.UseEndpoints(endpoints =>
